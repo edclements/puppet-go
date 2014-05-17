1aea56228adb07acc60c139852b9a35fa99d133a
edclements <ed.clements@gmail.com>
2014-05-17 20:33:13 +0100

    Use my fork of chgo
    I forked chgo because it had the wrong download URLs

--- manifests/init.pp
+++ manifests/init.pp
@@ -38,7 +38,7 @@ class go(
   repository { $chgo_root:
     ensure => $chgo_version,
     force  => true,
-    source => 'wfarr/chgo',
+    source => 'edclements/chgo',
     user   => $chgo_user,
   }
 
